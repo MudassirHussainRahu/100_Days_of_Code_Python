fac3a8c37b6e19b00ba7469607f3cfbba60188b6
MudassirHussainRahu <muddassir.rahu@gmail.com>
2025-09-23 00:09:07 +0500

    Code-Review-Suggestions

--- 15_Coffee_Machine.py
+++ 15_Coffee_Machine.py
@@ -33,16 +33,16 @@ profit = 0
 
 def report():
     print(f"---------Resources---------------")
-    print(f"Water: {resources["water"]}ml.")
-    print(f"Milk: {resources["milk"]}ml.")
-    print(f"Coffee: {resources["coffee"]}g.")
+    print(f"Water: {resources['water']}ml.")
+    print(f"Milk: {resources['milk']}ml.")
+    print(f"Coffee: {resources['coffee']}g.")
     print(f"Money: ${profit}")
 
 def check_resources(choice):
     ingredients = MENU[choice]["ingredients"]
     enough_resources = True
     for r in ingredients.keys():
-        print(ingredients[r] , resources[r])
+        # print(ingredients[r] , resources[r])
         if ingredients[r] > resources[r]:
             enough_resources = False
             print(f"Sorry there is not enough {r}")
@@ -68,31 +68,34 @@ def coffee():
 
     while True:
         choice = input( "What would you like? (espresso/latte/cappuccino):")
-
         if choice == "off":
+            report()
             print("Shutting Off.")
             break
 
         elif choice == "report":
             report()
         elif choice in MENU.keys():
-            print(choice)
+            # print(choice)
             if check_resources(choice):
-                quarters = float(input("Enter Quarters:"))
-                dimes = float(input("Enter Dimes:"))
-                nickles = float(input("Enter Nickles:"))
-                pennies = float(input("Enter Pennies:"))
-                money = calculate_money(quarters, dimes, nickles, pennies)
-                print(money)
-                if sufficiant_amount(choice, money):
-                    cost = MENU[choice]["cost"]
-                    change = round(money - cost,2)
-                    profit += cost
-                    deplete_resources(choice)
-                    if change > 0:
-                        print(f"Here is ${change} dollors in change.")
-                else:
-                    print("Sorry that's not enough money. Money refunded.")
+                try:
+                    quarters = int(input("Enter Quarters:"))
+                    dimes = int(input("Enter Dimes:"))
+                    nickels = int(input("Enter Nickels:"))
+                    pennies = int(input("Enter Pennies:"))
+                    money = calculate_money(quarters, dimes, nickels, pennies)
+                    # print(money)
+                    if sufficiant_amount(choice, money):
+                        cost = MENU[choice]["cost"]
+                        change = round(money - cost,2)
+                        profit += cost
+                        deplete_resources(choice)
+                        if change > 0:
+                            print(f"Here is ${change} dollars in change.")
+                    else:
+                        print("Sorry that's not enough money. Money refunded.")
+                except:
+                    print("Invalid Coins.")
         else:
             print("Invalid Input.")
 
