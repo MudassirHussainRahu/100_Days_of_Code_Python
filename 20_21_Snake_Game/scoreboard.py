964a7bbed4a8c198ce36a2968793354bd3b7f774
MudassirHussainRahu <muddassir.rahu@gmail.com>
2025-11-12 14:00:33 +0500

    high-score-save

--- 20_21_Snake_Game/scoreboard.py
+++ 20_21_Snake_Game/scoreboard.py
@@ -11,7 +11,8 @@ class ScoreBoard(Turtle):
         self.penup()
         self.goto(0,270)
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.hideturtle()
         self.update_scoreboard()
@@ -27,8 +28,11 @@ class ScoreBoard(Turtle):
     def refresh(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
+
 
 
     # def game_over(self):
